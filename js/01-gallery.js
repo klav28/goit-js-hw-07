54f4cc2f11bc2fef7065ad006830c8b5ceb5b5da
klav28 <klav28@gmail.com>
2023-03-05 14:30:03 +0200

    prefinal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,12 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const markupGallery = (images) => {
-    return images.map(({ preview, description }) => {
-        return `<div class="gallery__item"><img src=${preview} alt="${description}" class="gallery__image"/></div>`;
+    return images.map(({ preview, original, description }) => {
+        return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img src="${preview}" alt="${description}" class="gallery__image" data-source="${original}"/></a></div>`;
     }).join(" ");
 }
 
-// console.log(markupGallery(galleryItems));
-   
+const handleGallery = (ev) => {
+    ev.preventDefault();
+
+    if (ev.target.nodeName !== "IMG") {
+        return
+    }
+
+    const imageSrc = ev.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+        <img src=${imageSrc} width="1280">`);
+    instance.show();
+
+    window.addEventListener('keydown', handleKeydown);
+
+    function handleKeydown(ev) {
+        if (ev.code === 'Escape') {
+            instance.close();
+            window.removeEventListener('keydown', handleKeydown);
+        }
+    }
+}
+
 const galleryEl = document.querySelector(".gallery");
 galleryEl.innerHTML = markupGallery(galleryItems);
+
+galleryEl.addEventListener("click", handleGallery);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const markupGallery = (images) => {
+    return images.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    }).join(" ");
+}
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML = markupGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {captions: true, captionDelay:250, captionsData:"alt"});
+
+lightbox.on("shown.simplelightbox", window.addEventListener("keydown", handleKeydown));
+
+lightbox.on("closed.simplelightbox", window.removeEventListener("keydown", handleKeydown));
+
+function handleKeydown(ev) {
+    if (ev.code === 'Escape') {
+        lightbox.close();
+       }
+}
